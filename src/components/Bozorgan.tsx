b56daaaa9e531aa9e38eb29e8c6927d2a546e5fb
Msaeed144 <mohammadsaeed76@gmail.com>
2024-09-05 11:39:03 +0330

    create bozorgan component

--- src/components/Bozorgan.tsx
+++ src/components/Bozorgan.tsx
@@ -6,7 +6,7 @@ const Bozorgan = () => {
     const [ sokhan , setSokhan ] = useState<SokhanBozorgan>()
     useEffect(()=>{
         const getSokhan = async () => {
-            const result = await axios.get("https://one-api.ir/sokhan/?token=640504:66cc87985ca70&action=random")
+            const result = await axios.get("https://one-api.ir/sokhan/?token=640504:66d94f859e0be&action=random")
             setSokhan(result.data.result)
         }
         
@@ -17,7 +17,8 @@ const Bozorgan = () => {
     },[])
   return (
     <div className="p-3 text-center">
-        <h4 className=" mb-4 text-xl">هر وقت بیای اینجا میتونی روزت رو با یه حرف قشنگ بسازی...</h4>
+        <h4 className=" text-xl font-extrabold">هر وقت بیای اینجا میتونی روزت رو با یه حرف قشنگ بسازی...</h4>
+        <hr  className=" w-2/3 text-center mx-auto my-2 border-yellow-400"/>
         <p className="mb-4 text-sm">{sokhan?.text}</p>
         <p>{sokhan?.author}</p>
     </div>
