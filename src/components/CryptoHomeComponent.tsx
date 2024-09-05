17e451a981f07c26bc89fd19c54d3d25adc7377b
Msaeed144 <mohammadsaeed76@gmail.com>
2024-09-05 11:39:55 +0330

    crypto components created

--- src/components/CryptoHomeComponent.tsx
+++ src/components/CryptoHomeComponent.tsx
@@ -1,6 +1,38 @@
-const CryptoHomeComponent = () => {
+import { MarketData } from "../tools/interfaces"
+import { englishToPersian } from "replace-persian-number";
+import { Tooltip } from "@mui/material";
+
+interface CryptoHomeComponentProps {
+  crypto: MarketData;
+}
+
+const CryptoHomeComponent: React.FC<CryptoHomeComponentProps>  = ({crypto}) => {
+  const formatNumber = (value: number): string => {
+    // تبدیل عدد به رشته
+    const valueString = value.toString();
+    // استفاده از regex برای جدا کردن سه رقم سه رقم از راست
+    return valueString.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
   return (
-    <div>CryptoHomeComponent</div>
+    <Tooltip className=" cursor-pointer" title={`درصد تغییرات  ${crypto.stats["24h_ch"]}`} placement="top" arrow enterDelay={200} leaveDelay={100}>
+      <div className=" border border-[#b37cff] shadow-md p-4 text-center rounded-md">
+        <div className="flex items-center gap-3">
+          <div>
+            <p className=" text-[12px] text-secondcol">{crypto.baseAsset}/TMN</p>
+            <p className=" text-[12px] text-secondcolhov">{crypto.faBaseAsset}/تومان</p>
+            </div>
+          <div>
+            <img className="w-9" src={crypto.baseAsset_png_icon} alt="" />
+          </div>
+        </div>
+        <div>
+          <div className=" text-sm mt-2">
+            <p>آخرین قیمت :</p>
+            <p>{englishToPersian(String(formatNumber(Math.round(Number(crypto.stats.lastPrice)))))}</p>
+          </div>
+        </div>
+      </div>
+    </Tooltip>
   )
 }
 
--- src/components/CryptoHomePage.tsx
+++ src/components/CryptoHomePage.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react"
 import CryptoHomeComponent from "./CryptoHomeComponent"
+import axios from "axios"
+import { MarketData } from "../tools/interfaces";
 
 const CryptoHomePage = () => {
+  const [cryptos, setCryptos] = useState<MarketData[]>([]);
+
+  useEffect(()=>{
+    const getCryptos = async () => {
+      try {
+      const response = await axios.get('https://api.wallex.ir/v1/markets')
+      const data  = response.data.result.symbols
+      const formattedData: MarketData[] = Object.keys(data).map(key => data[key]);
+      const filteredData = formattedData.filter(
+        item => item.symbol === "BTCTMN" || item.symbol === "DOGSTMN" || item.symbol === "USDTTMN" ||
+        item.symbol === "ETHTMN" || item.symbol === "DOGETMN" || item.symbol === "SOLTMN" || item.symbol ==="NOTTMN"
+      );
+      setCryptos(filteredData);
+      } catch (error){
+        console.error('Error fetching crypto data:', error);
+      }
+    }
+    getCryptos()
+  },[cryptos])
   return (
-    <div>CryptoHomePage
-        <CryptoHomeComponent />
+    <div>
+        <div className=" flex flex-row-reverse gap-3 justify-center">
+          {cryptos.map(crypto => <CryptoHomeComponent key={crypto.symbol} crypto={crypto}/>)}
+        </div>
     </div>
   )
 }
