3e6af652f58ff1f432e1fa0032e1ab704d65b805
Msaeed144 <mohammadsaeed76@gmail.com>
2024-09-07 00:15:13 +0330

    crypto page in progress

--- src/pages/CryptoPage.tsx
+++ src/pages/CryptoPage.tsx
@@ -1,7 +1,12 @@
-
+import Progress from '../assets/inProgress.gif'
 function CryptoPage() {
   return (
-    <div>CryptoPage</div>
+    <div className='flex justify-center w-full h-full my-auto'>
+      <div>
+        <h1 className=' text-white text-center text-2xl mb-2'>قسمت رمز ارز در دست احداثه...</h1>
+        <img className='mx-auto rounded-xl text-center w-96 h-96' src={Progress} alt="" />
+      </div>
+    </div>
   )
 }
 
